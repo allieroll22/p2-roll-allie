fc02b8ee3c046cee83ddc4596113cceefc78d842
“Allie <allieroll22@ufl.edu>
2019-03-19 14:18:59 -0400

    Add photo folder

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var elementSpace = document.getElementById('place');
+/* var elementSpace = document.getElementById('place');
 
 var weatherSpace = document.getElementById('weather');
 
+
+// API Format
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -16,3 +18,4 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=32801,us&appid=6efff70fe1477748e31c17d1c504635f', true);
 xmlhttp.send();
+*/
