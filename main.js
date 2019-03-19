32a7ec259161a980107d7ce5ab780e3448b3f9e9
“Allie <allieroll22@ufl.edu>
2019-03-19 14:11:11 -0400

    Add starter files

---
+++ main.js
@@ -0,0 +1,18 @@
+var elementSpace = document.getElementById('place');
+
+var weatherSpace = document.getElementById('weather');
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        var cityName = document.createTextNode(apiResult.name);
+        elementSpace.appendChild(cityName);
+
+        var weatherToday = document.createTextNode(apiResult.weather[0].description);
+        weatherSpace.appendChild(weatherToday);
+    }
+};
+xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=32801,us&appid=6efff70fe1477748e31c17d1c504635f', true);
+xmlhttp.send();
